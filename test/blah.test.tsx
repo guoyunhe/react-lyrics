fc23ded8ae57338c920abcd4243f53896cdbc2dc
Guo Yunhe <i@guoyunhe.me>
2021-01-28 22:53:58 +0800

    fix test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Default as Thing } from '../stories/Thing.stories';
+import { Default as Lyrics } from '../stories/Lyrics.stories';
 
 describe('Thing', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<Lyrics />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
